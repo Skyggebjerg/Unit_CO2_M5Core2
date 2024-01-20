86a7697f71c5736716549882b92ff1a690e06a53
Skyggebjerg <ole@skyggebjerg.dk>
2024-01-20 22:47:31 +0100

    working

--- .pio/build/m5stack-core2/lib657/Sensirion Core/SensirionCrc.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/m5stack-core2/lib657/Sensirion\ Core/SensirionCrc.cpp.o: \
- .pio/libdeps/m5stack-core2/Sensirion\ Core/src/SensirionCrc.cpp \
- .pio/libdeps/m5stack-core2/Sensirion\ Core/src/SensirionCrc.h
